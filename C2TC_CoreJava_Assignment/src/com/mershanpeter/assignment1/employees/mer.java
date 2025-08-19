8c1e24ac3bfe413aee8f78e4c1ae6ac12c7838e5
mershanpeter <mershanpeter27@gmail.com>
2025-08-19 16:26:48 +0530

    bjvjy

---
+++ C2TC_CoreJava_Assignment/src/com/mershanpeter/assignment1/employees/mer.java
@@ -0,0 +1,16 @@
+package com.mershanpeter.assignment1.employees;
+
+public class mer {
+	package com.mershanpeter.assignment1.employees;
+	import java.util.Scanner;
+	public class kjkjl {
+		public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int m = sc.nextInt();
+		int sum = m+n;
+		System.out.println(sum);
+	}
+	}
+
+}
